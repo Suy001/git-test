eed9cc5aa49af951e079490da94c943fe2764451
Suy <Suy@qq.com>
2021-11-14 23:30:33 +0800

    first commit

---
+++ src/main/java/com/hd/GitTest.java
@@ -0,0 +1,11 @@
+package com.hd;
+
+/**
+ * @author Suy
+ * @create 2021-11-14 23:27
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
