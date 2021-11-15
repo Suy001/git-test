159248017ab38c574ce5587b029921f423b542a8
Suy001 <94269624+Suy001@users.noreply.github.com>
2021-11-15 14:33:19 +0800

    Update GitTest.java

--- src/main/java/com/hd/GitTest.java
+++ src/main/java/com/hd/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("update by hot-fix");
         System.out.println("update by master");
         System.out.println("update by master pull test");
+        System.out.println("github-update");
     }
 }
